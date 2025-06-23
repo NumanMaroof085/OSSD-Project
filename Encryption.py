a3b6aa79d83118cd46f6378b4feeab7d4f2ae59b
NumanMaroof085 <f2023408126@umt.edu.pk>
2025-06-23 15:52:21 +0500

    new file including hashlib

--- Encryption.py
+++ Encryption.py
@@ -1,5 +1,10 @@
 from cryptography.fernet import Fernet
+import hashlib as hl
 import dbfile as db
+
+def encrypt(text):
+    return hl.sha256(text.encode()).hexdigest()
+
 
 def encrypt_text(plain_text, key):
     cipher = Fernet(key)
@@ -11,23 +16,3 @@ def decrypt_text(encrypted_text, key):
     decrypted = cipher.decrypt(encrypted_text).decode()
     return decrypted
 
-if __name__ == "__main__":
-    key = db.read_pass(2)         # Example: [(2, 'TzJczx8gR...==')]
-    key1 = key[0][1].encode()     # Convert to bytes
-    print("Key:", key1)
-
-    check = db.read_pass(1)       # [(1, 'gAAAAABl...')]
-    print("Encrypted password:", check[0][1])
-
-    print("Key (bytes):", key1)
-    print("Length of key:", len(key1))
-    print("Encrypted password (raw):", check[0][1])
-    print("Encrypted password (type):", type(check[0][1]))
-    check2 = check[0][1]
-    check2 = check2.encode()
-    print("Encrypted password (raw):", check[0][1])
-    print("Encrypted password (type):", type(check[0][1]))
-
-
-    check1 = decrypt_text(check[0][1].encode(), key1)  # ✅ Encode before decrypt
-    print("Decrypted password:", check1)
